09f7b27af94331fde4dedf787ff4fd1526f057d9
Praveen Dubey <dubeypraveen1506@gmail.com>
2023-03-09 17:01:20 +0530

    default and static (variable) method in interface used

--- src/Praveen/MultipleInheritance.java
+++ src/Praveen/MultipleInheritance.java
@@ -2,6 +2,13 @@ package Praveen;
 
 interface Run {
     void run();
+    static int a= 5;
+    default void say(){
+        System.out.println("Welcome");
+    }
+    static void speak(){
+        System.out.println("The number is: "+a);
+    }
 }
 interface Walk{
     void walk();
@@ -11,8 +18,10 @@ class Humans extends Animals{
 }
 public class MultipleInheritance{
     public static void main(String[] args) {
+        Run.speak();
         Run r = new Humans();
         Walk w = new Humans();
+        r.say();
         r.run();
         w.walk();
     }
